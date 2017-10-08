3010e88e24ca82e5a2d06a4c92c39777a4aca048
Mike Huot <mhuot@mhuot.net>
2017-10-08 09:14:18 -0500

    Cleaned out a little code I was testing with. Still need to escape out alarm.description

--- bot.js
+++ bot.js
@@ -42,20 +42,6 @@ async function showAlarms  () {
   }
 }
 
-function resolveAfter2Seconds(x) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(x);
-    }, 2000);
-  });
-}
-
-async function add1(x) {
-  const a = await resolveAfter2Seconds(20);
-  const b = await resolveAfter2Seconds(30);
-  return x + a + b;
-}
-
 //////// Bot Kit //////
 
 const controller = Botkit.sparkbot({
